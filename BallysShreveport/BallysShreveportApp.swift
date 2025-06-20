60b4f426c73e88dc9b19e9c6cb9484165fba04ba
Developer <Lepiozdyx@gmail.com>
2025-06-20 13:03:59 -0300

    Initial Commit

---
+++ BallysShreveport/BallysShreveportApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallysShreveportApp.swift
+//  BallysShreveport
+//
+//  Created by Alex on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BallysShreveportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BallysShreveport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BallysShreveport
+//
+//  Created by Alex on 20.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
